497bf7a44832d04db8a8baee4ebe8e843193ef17
PriyanshuKSharma <priyanshu17ks@gmail.com>
2026-06-30 11:53:08 +0530

    chore: bump version to 1.1.0 and update default timezone to Asia/Kolkata

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-VERSION = "1.0.0"
+VERSION = "1.1.0"
 APP_TITLE = "AutoTest Studio"
 DEFAULT_BUS = "virtual"
 DEFAULT_CHANNEL = "vcan0"
 DB_PATH = "autoteststudio.db"
 THEME = "light"   # "dark" or "light"
-TIMEZONE = "UTC"  # IANA timezone name used for CAN log display
+TIMEZONE = "Asia/Kolkata"  # IST timezone name used for CAN log display
